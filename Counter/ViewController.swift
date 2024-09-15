02f114e95923adf5217c0b62fd3ca298baac29e0
KoLili12 <nickzhirnoff@gmail.com>
2024-09-15 16:36:27 +0300

    Первая версия

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,64 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var labelCounter: UILabel!
+    @IBOutlet weak var buttonPlus: UIButton!
+    @IBOutlet weak var buttonMinus: UIButton!
+    
+    @IBOutlet weak var historyChange: UITextView!
+    
+    
+    
+    
+    private var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        historyChange.isEditable = false
     }
-
-
+    
+    @IBAction func plusCounter(_ sender: Any) {
+        historyChange.text += "\n \(currentDate()): значение изменено на +1"
+        counter += 1
+        guard let pref = labelCounter.text?.prefix(19) else {
+            return
+        }
+        labelCounter.text = pref + String(counter)
+        
+    }
+    
+    @IBAction func minusCounter(_ sender: Any) {
+        if counter == 0 {
+            historyChange.text += "\n \(currentDate()): попытка уменьшить значение счётчика ниже 0"
+            return
+        }
+        else {
+            historyChange.text += "\n \(currentDate()): значение изменено на -1"
+            counter -= 1
+            guard let pref = labelCounter.text?.prefix(19) else {
+                return
+            }
+            labelCounter.text = pref + String(counter)
+        }
+    }
+    
+    
+    @IBAction func zeroingOut(_ sender: Any) {
+        historyChange.text += "\n \(currentDate()): значение сброшено"
+        counter = 0
+        guard let pref = labelCounter.text?.prefix(19) else {
+            return
+        }
+        
+        labelCounter.text = pref + String(counter)
+    }
+    
+    func currentDate() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "[dd.MM.yy hh:mm:ss]"
+        return dateFormatter.string(from: Date())
+    }
+    
 }
 
