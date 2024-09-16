27eb8d588237799982e98de09964b42199618839
KoLili12 <nickzhirnoff@gmail.com>
2024-09-16 10:04:18 +0300

    Правки

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,60 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var labelCounter: UILabel!
-    @IBOutlet weak var buttonPlus: UIButton!
-    @IBOutlet weak var buttonMinus: UIButton!
-    
-    @IBOutlet weak var historyChange: UITextView!
-    
-    
-    
-    
+    @IBOutlet weak private var counterLabel: UILabel!
+    @IBOutlet weak private var plusButton: UIButton!
+    @IBOutlet weak private var minusButton: UIButton!
+    @IBOutlet weak private var historyChangeTextView: UITextView!
     private var counter = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        historyChange.isEditable = false
+        historyChangeTextView.isEditable = false
     }
     
-    @IBAction func plusCounter(_ sender: Any) {
-        historyChange.text += "\n \(currentDate()): значение изменено на +1"
+    @IBAction private func plusCounter(_ sender: Any) {
+        historyChangeTextView.text += "\n \(currentDate()): значение изменено на +1"
         counter += 1
-        guard let pref = labelCounter.text?.prefix(19) else {
+        guard let pref = counterLabel.text?.prefix(19) else {
             return
         }
-        labelCounter.text = pref + String(counter)
-        
+        counterLabel.text = pref + String(counter)
     }
     
-    @IBAction func minusCounter(_ sender: Any) {
+    @IBAction private func minusCounter(_ sender: Any) {
         if counter == 0 {
-            historyChange.text += "\n \(currentDate()): попытка уменьшить значение счётчика ниже 0"
+            historyChangeTextView.text += "\n \(currentDate()): попытка уменьшить значение счётчика ниже 0"
             return
         }
         else {
-            historyChange.text += "\n \(currentDate()): значение изменено на -1"
+            historyChangeTextView.text += "\n \(currentDate()): значение изменено на -1"
             counter -= 1
-            guard let pref = labelCounter.text?.prefix(19) else {
+            guard let pref = counterLabel.text?.prefix(19) else {
                 return
             }
-            labelCounter.text = pref + String(counter)
+            counterLabel.text = pref + String(counter)
         }
     }
     
     
-    @IBAction func zeroingOut(_ sender: Any) {
-        historyChange.text += "\n \(currentDate()): значение сброшено"
+    @IBAction private func zeroingOut(_ sender: Any) {
+        historyChangeTextView.text += "\n \(currentDate()): значение сброшено"
         counter = 0
-        guard let pref = labelCounter.text?.prefix(19) else {
+        guard let pref = counterLabel.text?.prefix(19) else {
             return
         }
         
-        labelCounter.text = pref + String(counter)
+        counterLabel.text = pref + String(counter)
     }
     
-    func currentDate() -> String {
+    private func currentDate() -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "[dd.MM.yy hh:mm:ss]"
         return dateFormatter.string(from: Date())
